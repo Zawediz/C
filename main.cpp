5ddada2c52b6e218e4c4d3f0fddea0979ca6f775
Zawediz <bikbulatov.DI@phystech.edu>
2019-04-28 22:04:46 +0300

    Исправление ошибок

--- main.cpp
+++ main.cpp
@@ -4,8 +4,6 @@
 #include <climits>
 #include <set>
 
-const unsigned INF = INT_MAX;
-
 struct Dot
 {
     Dot (int a, int b): vertex(a), weight(b){}
@@ -13,10 +11,10 @@ struct Dot
     int weight;
 };
 
-class Graph
+class CGraph
 {
 public:
-    Graph(int numOfVertices) : AmountOfVertices(numOfVertices), lists(numOfVertices) {}
+    CGraph(int numOfVertices) : AmountOfVertices(numOfVertices), lists(numOfVertices) {}
 
     virtual void AddEdge(int from, int to, int weight);
 
@@ -29,54 +27,54 @@ private:
     std::vector<std::list<Dot>> lists;
 };
 
-void Graph::AddEdge(int from, int to, int weight)
+void CGraph::AddEdge(int from, int to, int weight)
 {
     lists[to].push_back(Dot(from, weight));
     lists[from].push_back(Dot(to, weight));
 }
 
-int Graph::VerticesCount() const
+int CGraph::VerticesCount() const
 {
     return AmountOfVertices;
 }
 
 
-void Graph::GetRelated(int vertex, std::vector<Dot> &vertices) const
+void CGraph::GetRelated(int vertex, std::vector<Dot> &vertices) const
 {
     vertices.clear();
     for (const Dot &i : lists[vertex])
         vertices.push_back(i);
 }
 
-int Dijkstra(Graph const &graph, int from, int to)
+int FindMinWays(const CGraph &graph, int from, int to)
 {
-    std::set<std::pair<int, int>> PairSet;
-    PairSet.emplace(std::make_pair(0, from));
+    std::set<std::pair<int, int>> waySet;
+    waySet.emplace(std::make_pair(0, from));
 
-    std::vector<int> way(graph.VerticesCount(), INF);
+    std::vector<int> way(graph.VerticesCount(), INT_MAX);
     way[from] = 0;
 
-    while (PairSet.size() != 0)
+    while (waySet.size() != 0)
     {
-        int v = (PairSet.begin())->second;
+        int v = (waySet.begin())->second;
         std::vector<Dot> ways;
         graph.GetRelated(v, ways);
-        PairSet.erase(PairSet.begin());
+        waySet.erase(waySet.begin());
 
         for (Dot p : ways)
         {
             if (way[p.vertex] > p.weight + way[v])
             {
-                if (way[p.vertex] != INF)
-                    PairSet.erase(std::make_pair(way[p.vertex], p.vertex));
+                if (way[p.vertex] != INT_MAX)
+                    waySet.erase(std::make_pair(way[p.vertex], p.vertex));
 
                 way[p.vertex] = p.weight + way[v];
-                PairSet.emplace(std::pair<int, int>(way[p.vertex], p.vertex));
+                waySet.emplace(std::pair<int, int>(way[p.vertex], p.vertex));
             }
         }
     }
 
-    if (way[to] != INF)
+    if (way[to] != INT_MAX)
         return way[to];
     else
         return -1;
@@ -85,17 +83,18 @@ int Dijkstra(Graph const &graph, int from, int to)
 
 int main()
 {
-    int N, M, f, t;
+    int N, M;
     std::cin >> N;
     std::cin >> M;
-    Graph graph(N);
+    CGraph graph(N);
     for (int i = 0; i < M; ++i)
     {
         int a, b, c;
         std::cin >> a >> b >> c;
         graph.AddEdge(a, b, c);
     }
+    int f, t;
     std::cin >> f >> t;
-    std::cout << Dijkstra(graph, f, t);
+    std::cout << FindMinWays(graph, f, t);
     return 0;
 }
