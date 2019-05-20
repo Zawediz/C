4f42b9ee508c05aad4decba54941677829e9bf5a
Zawediz <bikbulatov.DI@phystech.edu>
2019-05-21 00:39:44 +0300

    добавление файлов

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <string>
 #include <random>
+#include <memory>
 
 struct Node {
     Node() : str(""), key(1), priority(rand()), left(nullptr), right(nullptr) {}
@@ -9,34 +10,35 @@ struct Node {
     Node(std::string s) : str(s), key(1), priority(rand()), left(nullptr), right(nullptr) {}
 
     std::string str;
-    Node *left;
-    Node *right;
+    std::shared_ptr<Node> left;
+    std::shared_ptr<Node> right;
     int priority;
     int key;
+
 };
 
-int getKey(Node *node) {
+int getKey(std::shared_ptr<Node> &node) {
     if (!node) {
         return 0;
     } else {
-        return node->key;
+        return node.get()->key;
     }
 }
 
 
-void updateKey(Node *&node) {
+void updateKey(std::shared_ptr<Node> &node) {
     if (node != nullptr) {
         node->key = getKey(node->left) + getKey(node->right) + 1;
     }
 }
 
-void split(Node *node, Node *&left, Node *&right, int pos) {
+void split(std::shared_ptr<Node> node, std::shared_ptr<Node> &left, std::shared_ptr<Node> &right, int pos) {
     if (!node) {
         left = nullptr;
         right = nullptr;
     } else if (getKey(node->left) >= pos) {
         right = node;
-        split(right->left, left, right->left, pos);
+        split(right.get()->left, left, right.get()->left, pos);
         updateKey(right);
         updateKey(left);
     } else {
@@ -48,7 +50,7 @@ void split(Node *node, Node *&left, Node *&right, int pos) {
 }
 
 
-void merge(Node *&node, Node *left, Node *right) {
+void merge(std::shared_ptr<Node> &node, std::shared_ptr<Node> left, std::shared_ptr<Node> right) {
     if (!right) {
         node = left;
         updateKey(node);
@@ -68,29 +70,35 @@ void merge(Node *&node, Node *left, Node *right) {
     }
 }
 
-void InsertAt(Node *&node, int position, const std::string &value) {
-    Node *left = nullptr;
-    Node *right = nullptr;
+void InsertAt(std::shared_ptr<Node> &node, int position, const std::string &value) {
+    auto left = std::make_shared<Node>();
+    auto right = std::make_shared<Node>();
+    left = nullptr;
+    right = nullptr;
     split(node, left, right, position);
-    node = new Node(value);
-    merge(node, node, right);
-    merge(node, left, node);
+    auto node2 = std::make_shared<Node>(value);
+    merge(node2, node2, right);
+    merge(node, left, node2);
 }
 
-void DeleteAt(Node *&node, int position) {
-    Node *left = nullptr;
-    Node *middle = nullptr;
-    Node *right = nullptr;
-    split(node, left, middle, position);
-    split(middle, middle, right, 1);
+
+void DeleteAt(std::shared_ptr<Node> &node, int position_left, int position_right) {
+    auto left = std::make_shared<Node>();
+    auto middle = std::make_shared<Node>();
+    auto right = std::make_shared<Node>();
+    left = nullptr;
+    middle = nullptr;
+    right = nullptr;
+    split(node, left, middle, position_left);
+    split(middle, middle, right, position_right - position_left + 1);
     merge(node, left, right);
 }
 
 
-std::string GetAt(Node *node, int position) {
+std::string GetAt(std::shared_ptr<Node> node, int position) {
     int key = getKey(node->left);
     if (key == position) {
-        return node->str;
+        return node.get()->str;
     } else if (key < position) {
         return GetAt(node->right, position - key - 1);
     } else return GetAt(node->left, position);
@@ -99,12 +107,12 @@ std::string GetAt(Node *node, int position) {
 int main() {
     int k;
     std::cin >> k;
-    Node *a = nullptr;
+    auto a = std::make_shared<Node>();
+    a = nullptr;
     for (int i = 0; i < k; ++i) {
         char command;
         int position;
         std::cin >> command >> position;
-
         if (command == '+') {
             std::string s;
             std::cin >> s;
@@ -112,11 +120,11 @@ int main() {
         } else if (command == '-') {
             int position2;
             std::cin >> position2;
-            for (int j = position; j <= position2; ++j)
-                DeleteAt(a, j);
+            DeleteAt(a, position, position2);
         } else if (command == '?') {
             std::cout << GetAt(a, position) << std::endl;
         }
     }
+
     return 0;
 }
